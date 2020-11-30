50219b78c06f69b97db7ceb4b8fe682ee8224a4c
Alex <dev.alex.88@gmail.com>
2020-11-30 17:26:29 +0700

    rename

--- src/config/common.ts
+++ src/config/common.ts
@@ -3,14 +3,14 @@ const REPO_URL = "https://github.com/katalon-studio/katalium-vscode-extension";
 const CREATE_TEST_CASE_URL = "https://docs.katalon.com/katalium-framework/docs/katalium-framework-create-test-case.html";
 const CREATE_PAGE_URL = "https://docs.katalon.com/katalium-framework/docs/katalium-framework-page-object-model.html";
 const RECORD_GENERATE_CODE_URL = "https://chrome.google.com/webstore/detail/katalon-recorder-selenium/ljdobmomdgdljniojadhoplhkpialdid";
-const KATA_SERVER_STANALONE_JAR_URL = "https://github.com/katalon-studio/katalium-server/releases/download/v1.1.0/kata-server.jar";
+const KATA_SERVER_STANDALONE_JAR_URL = "https://github.com/katalon-studio/katalium-server/releases/download/v1.1.0/kata-server.jar";
 const PORT = 4444;
 const SERVER_JAR_FILE_NAME = "katalium-server-v1.1.0.jar";
 
 export {
   SAMPLE_PROJECT_URL,
   REPO_URL,
-  KATA_SERVER_STANALONE_JAR_URL as KATA_SERVER_STANDALONE_JAR_URL,
+  KATA_SERVER_STANDALONE_JAR_URL,
   CREATE_TEST_CASE_URL,
   CREATE_PAGE_URL,
   RECORD_GENERATE_CODE_URL,
